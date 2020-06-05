238c69d4ecf7fd02f38da96938a767a254eb4311
sayalipawade <sayalipawade05@gmail.com>
2020-06-05 19:24:53 +0530

    Added:Calculating monthly wage of employee

--- employee-wage.py
+++ employee-wage.py
@@ -6,23 +6,31 @@ WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 IS_EMP_PRESENT=1
 IS_EMP_PART_TIME=2
+WORKING_DAYS=20
 
 #variables
 emp_hours=0
 emp_daily_wage=0
+emp_monthly_wage=0
 
 class Employee:
 
     #Checking employee is present,absent or part time
     def checking_emp_attendance(self):
         global emp_hours
-        emp_check=random.randint(0,2)
-        if emp_check==IS_EMP_PRESENT:
-            emp_hours=8
-        elif emp_check==IS_EMP_PART_TIME:
-            emp_hours=4
-        emp_daily_wage=emp_hours*WAGE_PER_HOUR
-        print("Employee Daily Wage:",emp_daily_wage)
+        global emp_monthly_wage
+        for day in range(0,WORKING_DAYS):
+            emp_check=random.randint(0,2)
+            if emp_check==IS_EMP_PRESENT:
+                emp_hours=8
+            elif emp_check==IS_EMP_PART_TIME:
+                emp_hours=4
+            
+            #Calculating daily wage of employee
+            emp_daily_wage=emp_hours*WAGE_PER_HOUR
+            #Calculating monthly wage of employee
+            emp_monthly_wage=emp_monthly_wage+emp_daily_wage
+        print("Employee Monthly wage:",emp_monthly_wage)
 
 #Creating object of Employee class
 employee=Employee()
