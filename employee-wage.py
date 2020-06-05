37abbd57e5a985cde7cf398d6c3bc59b4cfc5a33
sayalipawade <sayalipawade05@gmail.com>
2020-06-05 18:44:54 +0530

    Added:Code to calculating daily wage of employee

--- employee-wage.py
+++ employee-wage.py
@@ -1,13 +1,23 @@
 import random
 print("Welcome to Employee Wage Computation")
 
+#Constants
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+IS_EMP_PRESENT=1
+
+#variables
+emp_hours=0
+emp_daily_wage=0
+
 class Employee:
     def checking_emp_present_or_absent(self):
+        global emp_hours
         emp_check=random.randint(0,1)
-        if emp_check==1:
-            print("Employee is present")
-        else:
-            print("Employee is absent")
+        if emp_check==IS_EMP_PRESENT:
+            emp_hours=8
+        emp_daily_wage=emp_hours*WAGE_PER_HOUR
+        print("Employee Daily Wage:",emp_daily_wage)
 
 employee=Employee()
 employee.checking_emp_present_or_absent()
